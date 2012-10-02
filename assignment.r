4a43015b0832b6876a95adf5a1533c2dd6c928c4
Aron Lindberg <aron.lindberg@case.edu>
2012-10-02 16:04:21 -0400

    Set up code from before

---
+++ assignment.r
@@ -0,0 +1,16 @@
+# Load necessary libraries & datasets
+library(TraMineR)
+data(biofam)
+
+# Parse and label the data
+biofam$chort <- cut(biofam$birthyr, c(1900,1930,1940,1950,1960), labels=c("1900-1929", "1930-1939", "1940-1949", "1950-1959"), right=FALSE)
+
+bf.states <- c("Parent", "Left", "Married", "Left/Married", "Child", "Left/Child", "Left/Married/Child", "Divorced")
+
+bf.shortlab <- c("P", "L", "M", "LM", "C", "LC", "LMC", "D")
+
+# Create a sequence object
+biofam.seq <- seqdef(biofam[,10:25], states=bf.shortlab, labels=bf.states, weights=biofam$wp00tbgs)
+
+# Computing longitudinal characteristics
+
